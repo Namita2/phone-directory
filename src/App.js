fdc0feed1a86b25706c5dc16764b2e947f9f3c40
Namita Banakar <namitabanakar@Namitas-MacBook-Air.local>
2020-04-14 18:47:41 +0530

    created skeleton of Home page

--- src/App.js
+++ src/App.js
@@ -3,8 +3,19 @@ import React from 'react';
 function App() {
   return (
     <div>
-      phone directory
+      <label htmlFor="Name">Name:</label>
+      <input id="name" type="text" placeholder="Type Here" defaultValue="Namita"/>
     </div>
+    // <div>
+    // <div className="header">
+    //   phone directory
+    // </div>
+    // <button>Add</button>
+    // <div>
+    //   <span>Name</span><br/>
+    //   <span>Number</span>
+    // </div>
+    // </div>
   );
 }
 
